a60c54edb38369e4aeca1813903fa42cc921ea82
Abhishek <abmishra700@gmail.com>
2022-04-25 14:53:33 +0530

    adding style

--- app.js
+++ app.js
@@ -8,17 +8,15 @@ const shopRoutes = require("./routes/shop");
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // orders is matter of the order of the routes
 //http://localhost:3000/admin/add-product
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-// 404 error
 app.use((req, res, next) => {
-  res
-    .status(404)
-    .sendFile(path.join(__dirname,  "views", "page-notFound.html"));
+  res.status(404).sendFile(path.join(__dirname, "views", "page-notFound.html"));
 });
 
 app.listen(port, () => {
