12109c7ad2860f3bf2eb8edc4aeb28ef73d7094a
Abhishek <abmishra700@gmail.com>
2022-04-25 12:27:08 +0530

    adding error page 404

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+// 404 error
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
+
 app.listen(port, () => {
   console.log(`Example 
   app listening at 
