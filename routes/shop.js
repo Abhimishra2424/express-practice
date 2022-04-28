b23e71cd73dd444b621eebded4c753fc7d3b1631
Abhishek <abmishra700@gmail.com>
2022-04-28 07:44:49 +0530

    Registering the Routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,11 +1,17 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const productsController = require('../controllers/products');
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get("/", shopController.getInedx);
+
+router.get("/products", shopController.getProducts);
+
+router.get("/cart", shopController.getCart);
+
+router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
