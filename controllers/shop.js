21bd3122b458971508205b9f134690faabc7023b
Abhishek <abmishra700@gmail.com>
2022-04-29 14:49:19 +0530

    rendering The Product Detail View

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,15 +12,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
-  res.redirect("/");
   Product.findById(prodId, (product) => {
-    // res.render("shop/product-detail", {
-    //   product: product,
-    //   pageTitle: product.title,
-    //   path: "/products",
-    // });
-    console.log(product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/products",
+    });
   });
 };
 
