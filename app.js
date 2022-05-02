1178e2fd5d5e2b3a1fea8e50f22ddc44a83ca566
Abhishek <abmishra700@gmail.com>
2022-05-02 23:02:26 +0530

    syncing js definitions to the database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const db = require("./util/database");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -11,7 +11,6 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -23,4 +22,12 @@ app.use((req, res, next) => {
   res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
-app.listen(5000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
