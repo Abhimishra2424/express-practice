5418c4ba756b58f9e0d2b4515ed3b25bb5905e26
Abhishek <abmishra700@gmail.com>
2022-05-02 20:43:31 +0530

    adding sequelize and do connection

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize("node", "root", "root", {
   host: "localhost",
-  user: "root",
-  database: "node",
-  password: "root",
+  dialect: "mysql",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
