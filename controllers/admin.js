288214e5ebb19e0fa6065ef317260ad56ecaf7fb
Abhishek <abmishra700@gmail.com>
2022-05-04 16:42:02 +0530

    updating products with sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -8,18 +8,24 @@ exports.getAddProduct = (req, res, next) => {
   });
 };
 
-exports.postAddProduct = async (req, res, next) => {
+exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
 
-  await Product.create({
+  Product.create({
     title: title,
     price: price,
     imageUrl: imageUrl,
     description: description,
-  });
+  })
+    .then(() => {
+      res.redirect("/products");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = async (req, res, next) => {
@@ -48,24 +54,32 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect("/admin/products");
+
+  Product.findByPk(prodId)
+    .then((product) => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.imageUrl = updatedImageUrl;
+      product.description = updatedDesc;
+      return product.save();
+    })
+    .then((r) => {
+      console.log("updated product");
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
 
-exports.getProducts = async (req, res, next) => {
-  const products = await Product.findAll();
-  res.render("admin/products", {
-    prods: products,
-    pageTitle: "Admin Products",
-    path: "/admin/products",
-  });
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.render("admin/products", {
+        prods: products,
+        pageTitle: "Admin Products",
+        path: "/admin/products",
+      });
+    })
+    .catch((err) => console.log(err));
 };
 
 exports.postDeleteProduct = async (req, res, next) => {
