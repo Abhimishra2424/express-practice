9cd839f3d391aadc41fb289c28087a26d3a8bb20
Abhishek <abmishra700@gmail.com>
2022-05-05 16:30:34 +0530

    One to many relationship and many to many relationship

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require("body-parser");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -39,9 +41,16 @@ Product.belongsTo(User, {
 });
 User.hasMany(Product);
 
+// One to many relationship and many to many relationship
+User.hasOne(Cart);
+Cart.belongsTo(User);
+// One to many relationship and many to many relationship
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1).then((user) => {
       if (!user) {
