c6a65bbe7543387fc78ac87faca753773995d8ee
Abhishek <abmishra700@gmail.com>
2022-05-05 16:05:20 +0530

    using Magic Association Methods

--- app.js
+++ app.js
@@ -17,6 +17,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -31,9 +40,18 @@ Product.belongsTo(User, {
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
+  // .sync({ force: true })
+  .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1).then((user) => {
+      if (!user) {
+        return User.create({ name: "Abhi", email: "abhi@gmail.com" });
+      }
+      return Promise.resolve(user);
+    });
+  })
+  .then((user) => {
+    // console.log(user);
     app.listen(3000);
   })
   .catch((err) => {
